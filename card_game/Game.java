33b5c7ead0df94fda9816d5a2db3d1cc60ab8092
Alan Russell <ajrussellaudio@gmail.com>
2016-08-28 00:07:42 +0100

    add players to game

--- card_game/Game.java
+++ card_game/Game.java
@@ -1,6 +1,6 @@
 // init deck
-// init players (array with dealer last)
 // shuffle deck
+// init players (array with dealer last)
 // (bet)
 // deal cards
 // check if any players have blackjack (pay out)
@@ -10,4 +10,53 @@
 // compare totals (each player to dealer)
 // go to (bet) (start draw)
 
+package card_game;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Game {
+
+  private Deck deck;
+  private ArrayList<Player> players = new ArrayList<Player>();
+  Scanner userInput = new Scanner(System.in);
+
+  public Game() {
+    this.deck = initDeck();
+    // initPlayers();
+  }
+
+  public void initPlayers(int numPlayers) {
+    if(numPlayers == 0){
+      numPlayers = getNumOfPlayers();
+    }
+    for(int i = 0; i < numPlayers; i++){
+      this.players.add(new HumanPlayer());
+    }
+    this.players.add(new Dealer());
+  }
+
+  // private
+
+  private Deck initDeck() {    
+    Deck newDeck = new Deck();
+    newDeck.fullDeck();
+    return newDeck;
+  }
+
+  private int getNumOfPlayers() { // for live 
+    System.out.println("\nHow many players?");
+    int numPlayers = Integer.parseInt(userInput.next());
+    return numPlayers;
+  }
+
+  // for tests 
+
+  public Deck deck() { // for testing
+    return deck;
+  }
+
+  public ArrayList players() {
+    return players;
+  }
+
+}
--- specs/GameTest.java
+++ specs/GameTest.java
@@ -4,6 +4,24 @@ import card_game.*;
 
 public class GameTest {
 
-  
-  
+  Game game;
+
+  @Before
+  public void before() {
+    this.game = new Game();
+  }
+
+  @Test
+  public void has52CardDeck() {
+    Deck deck = this.game.deck();
+    assertEquals(52, deck.getCount());
+  }
+
+  @Test
+  public void canInitPlayers() {
+    int numPlayers = 6;
+    game.initPlayers(numPlayers);
+    assertEquals( 7, game.players().size() ); // 6 + 1 Dealer
+  }
+
 }
